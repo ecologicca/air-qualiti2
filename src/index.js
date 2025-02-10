968ce145866d12965204e86035e0af866a418356
Casey R <casey.reid@hotmail.com>
2025-02-10 13:47:50 -0500

    Forgot Password Flow (buggy)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MainRouter from './MainRouter'; // Ensure you import your MainRouter
+import App from './App'; // Change this to import App instead of MainRouter
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainRouter />
+    <App /> {/* Render App instead of MainRouter */}
   </React.StrictMode>
 );
